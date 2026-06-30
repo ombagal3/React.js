c5cb18cc839f82ded80f84edf5eae1ff98384dc6
Om Bagal <ombagal8@gmail.com>
2026-07-01 00:13:42 +0530

    ...

--- json-apk/src/screen/Home.jsx
+++ json-apk/src/screen/Home.jsx
@@ -1,4 +1,3 @@
-
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
@@ -23,7 +22,9 @@ export default function Home() {
   const fetchUsers = async () => {
     const res = await axios.get("http://localhost:3000/users");
     setUser(res.data);
+    console.log(setUser);
   };
+
 
   const handleSendUser = async () => {
     await axios.post("http://localhost:3000/users", data);
@@ -37,62 +38,47 @@ export default function Home() {
   };
 
   const handleUpdate = async () => {
-    await axios.put(
-      "http://localhost:3000/users/" + data.id,
-      data
-    );
+    await axios.put("http://localhost:3000/users/" + data.id, data);
 
     fetchUsers();
     setData({ name: "", role: "" });
   };
 
-
-
-
   useEffect(() => {
     fetchUsers();
 
+    const timer = setInterval(() => {
+      setCurrentTime(new Date());
+    }, 1000);
 
-      const timer = setInterval(() => {
-    setCurrentTime(new Date());
-  }, 1000);
-
-  return () => clearInterval(timer);
+    return () => clearInterval(timer);
   }, []);
 
   const date = currentTime.toLocaleDateString("en-IN", {
-  day: "numeric",
-  month: "short",
-  year: "numeric",
-});
+    day: "numeric",
+    month: "short",
+    year: "numeric",
+  });
 
-const time = currentTime.toLocaleTimeString("en-IN");
+
+
+  const time = currentTime.toLocaleTimeString("en-IN");
 
   return (
     <div className="app">
-
       <aside className="sidebar">
         <h2>Notes</h2>
 
-        <button
-          className="plus"
-          onClick={handleSendUser}
-        >
+        <button className="plus" onClick={handleSendUser}>
           <i className="bi bi-plus-lg"></i>
         </button>
       </aside>
 
       <div className="main">
-
         <h1>Notes</h1>
 
         <div className="formBox">
-
-          <input
-            value={data.name}
-            onChange={getName}
-            placeholder="Title"
-          />
+          <input value={data.name} onChange={getName} placeholder="Title" />
 
           <input
             value={data.role}
@@ -100,23 +86,15 @@ const time = currentTime.toLocaleTimeString("en-IN");
             placeholder="Description"
           />
 
-          <button onClick={handleSendUser}>
-            Add
-          </button>
+          <button onClick={handleSendUser}>Add</button>
 
-          <button
-            className="updateBtn"
-            onClick={handleUpdate}
-          >
+          <button className="updateBtn" onClick={handleUpdate}>
             Update
           </button>
-
         </div>
-
+ 
         <div className="cards">
-
           {users.map((user, index) => (
-
             <div
               key={user.id}
               className="card"
@@ -124,50 +102,29 @@ const time = currentTime.toLocaleTimeString("en-IN");
                 background: colors[index % colors.length],
               }}
             >
-
               <h3>{user.name}</h3>
 
               <p>{user.role}</p>
 
-
-  
-
-              <div className="iconBox d-flex justify-content-between">
-
-
-                            <div className="timeBox">
-    <p>{date}</p>
-
-</div>
-
-                <div className="">
-                  <button
-                  onClick={() => updateFild(user)}
-                >
-                  <i className="bi bi-pencil-fill"></i>
-                </button>
-
-                <button
-                  onClick={() => deleteUser(user.id)}
-                >
-                  <i className="bi bi-trash-fill"></i>
-                </button>
+              <div className="iconBox d-flex justify-content-around ">
+                <div className="timeBox ">
+                  <p>{date}</p>
                 </div>
 
+                <div className="">
+                  <button className="" onClick={() => updateFild(user)}>
+                    <i className="bi bi-pencil-fill"></i>
+                  </button>
+
+                  <button onClick={() => deleteUser(user.id)}>
+                    <i className="bi bi-trash-fill"></i>
+                  </button>
+                </div>
               </div>
-
-              
-
             </div>
-
-            
-
           ))}
-
-       </div>
-
+        </div>
       </div>
-
     </div>
   );
 }
